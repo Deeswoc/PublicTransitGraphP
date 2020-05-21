3fd5eb6c338eb097acef05c932e016c18da29360
Deeswoc <47671781+Deeswoc@users.noreply.github.com>
2020-05-21 06:20:02 -0500

    mend

---
+++ transit-app/src/Components/FetchedTowns.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import d from '../devURL';
+import TownTable from './TownTable';
+
+
+class FetchedTowns extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            towns: []
+        };
+    }
+
+    async componentDidMount() {
+        const res = await fetch(`${d}/town/get-towns`);
+        console.log(res);
+        const towns = await res.json();
+        this.setState({
+            towns
+        });
+    }
+
+    render() {
+        return (
+         <div>
+            <TownTable townList={this.state.towns.map(town=>({...town, categories:['']}))}/>
+         </div>
+        );
+    }
+}
+
+
+
+export default FetchedTowns;
---
+++ transit-app/src/Routes/AddTowns/NewTownForm.js
@@ -0,0 +1,94 @@
+import React, { Component, useEffect, useContext } from 'react'
+import PropT from 'prop-types';
+import MultiSelWindow from '../../Components/MultiSelectWindow'
+import { NewTownFormContext } from '../../Contexts/NewTownFormContext';
+import { actions } from '../../Contexts/NewTownFormContextReducers/newTownFormContextReducer';
+import TownTable from '../../Components/TownTable';
+
+
+class NewTownForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            town: '',
+            parish: '',
+            category: ''
+        }
+
+    } 
+    form;
+    render(){
+        return(
+        <NewTownFormContext.Consumer>{(context) => {
+            const {
+                townsToSubmit,
+                parish,
+                name,
+                category,
+                submit,
+                dispatch,
+            } = context;
+            return(
+                <form ref={(form)=>this.form = form} onSubmit={submit}>
+                    <label htmlFor="Name">Name: </label>
+                    <input id="Name" type="Text" value={name} onChange={
+                        e => {
+                            dispatch({type: actions.STATE, $: {name: e.target.value}});
+                        }
+                    } required/>
+                    <label htmlFor="Parish">Parish: </label>
+                    <input id="Parish" type="Text" value = {parish}onChange={
+                        e => {
+                            dispatch({type: actions.STATE, $: {parish: e.target.value}});
+                        }
+                    } required/>
+                    <LocCat></LocCat>
+                    <MultiSelWindow/>
+                    <button type="button" onClick={
+                        (e)=>{
+                            dispatch({type:actions.ADD_CATEGORY, $: {category}
+                        })}}>Add Cat</button>
+                    <button type="button" onClick={
+                        (e) => {
+                            dispatch({type:actions.ADD_TOWN_SUBMITION});
+                            dispatch({type:actions.RESET_FORM});
+                        }
+                    }>Add Town</button>
+                    <button type="submit" onClick={submit}>Submit</button>
+                    <TownTable townList = {townsToSubmit}/>
+
+                </form>
+            )
+        }}</NewTownFormContext.Consumer>
+        );
+    }
+}
+
+
+
+function LocCat(props){
+    
+    const { catList, dispatch } = useContext(NewTownFormContext);
+    const onChange = (e) => {
+        dispatch({type:actions.STATE, $: {category: e.target.value}});
+    }
+
+    useEffect(()=>{
+        dispatch({type:actions.STATE, $: {category: catList[0]}});
+    },[catList, dispatch]);
+    return(
+        <select defaultValue={catList[0]} style={{minWidth: 110}} onChange={onChange}>
+            {catList.sort().map(category => (<option value={category}>{category}</option>))}
+        </select>
+    )
+}
+
+
+
+NewTownForm.propTypes = {
+    town: PropT.string,
+    parish: PropT.string
+}
+
+
+export default NewTownForm;
