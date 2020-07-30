7a4324934cf861b65eb4f7744a45a3565e424739
root <root@DESKTOP-SM79K3H.localdomain>
2020-07-30 16:10:03 -0400

    added a config to listen to nodemon. Removed the comments form api/routes/towns.js

--- api/routes/towns.js
+++ api/routes/towns.js
@@ -4,13 +4,6 @@ let router = express.Router();
 let categoriesRouter = require('./categories');
 let townController = require('../controllers/townController');
 
-// router.post('/add-town', townController.add_new_town);
-// router.post('/add-towns', townController.add_new_towns);
-// router.get('/get-towns', townController.get_towns)
-// router.get('/get-categories', townController.get_categories);
-// router.get('/get-towns/:id', townController.get_town)
-// router.get('/out-bound-routes/', townController.get_out_bound_routes);
-
 router.use('/categories', categoriesRouter);
 router.post('/', townController.add_new_towns);
 router.get('/', townController.get_towns);
