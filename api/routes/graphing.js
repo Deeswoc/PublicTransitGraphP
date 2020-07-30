7d8a6d59f3245a66775dcfc5d5d2b5c4654e9762
root <root@DESKTOP-SM79K3H.localdomain>
2020-07-30 04:14:53 -0400

    created a seperate file to handle db auth

--- api/routes/graphing.js
+++ api/routes/graphing.js
@@ -1,9 +1,5 @@
 const neo4j = require('neo4j-driver');
-const uri = 'bolt://localhost:7687';
-const user = 'neo4j';
-// const password = 'testt3$t56%';
-const password = '12345678'; 
-const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+const driver = require('../config/database');
 function addTownTransaction(tx, townName){
     return tx.run(
         'MERGE (a:Town {Name: $name}) ' + 
