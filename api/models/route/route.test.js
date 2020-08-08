aba30ddeb54c0a6429b4630645744f5378d0b778
root <root@DESKTOP-SM79K3H.localdomain>
2020-08-08 05:50:56 -0400

    added test for validate towns

--- api/models/route/route.test.js
+++ api/models/route/route.test.js
@@ -31,3 +31,36 @@ test('create a route in the database', async () => {
     }
     expect(await addRoute(newRoute)).toEqual(newRoute);
 })
+
+test('State if any element is missing from the passed elements', async () => {
+
+    const mockIDs = [1, 2, 3, 4, 5, 6];
+    let getTown = async (ID)=>{
+        if(mockIDs.includes(ID))
+            return {ID};
+    }
+    validateTowns = routeModel.validateTowns(getTown, routeModel.NotFound);
+
+    expect(await validateTowns(1, 3)).toBe(true);
+
+    try{
+        await validateTowns(4, 8);
+    }catch(error){
+        expect(error).toBeInstanceOf(routeModel.NotFound);
+        expect(error.missing).toEqual([8]);
+    }
+
+    try{
+        await validateTowns(23, 1);
+    }catch(error){
+        expect(error).toBeInstanceOf(routeModel.NotFound);
+        expect(error.missing).toEqual([23]);
+    }
+
+    try{
+        await validateTowns(23, 100);
+    }catch(error){
+        expect(error).toBeInstanceOf(routeModel.NotFound);
+        expect(error.missing).toEqual([23, 100]);
+    }
+})
