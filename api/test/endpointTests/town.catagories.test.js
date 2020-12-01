8beee74c4f14906e3b517bd698d6e000ac5b6b01
root <root@DESKTOP-SM79K3H.localdomain>
2020-11-30 22:38:09 -0500

    added eslint to the project

--- api/test/endpointTests/town.catagories.test.js
+++ api/test/endpointTests/town.catagories.test.js
@@ -1,83 +1,79 @@
-const { assert } = require('chai');
-let chai = require('chai');
-let chathttp = require('chai-http');
-let server = require('../../app');
-let driver = require('../../config/database');
-let expect = chai.expect;
-let should = chai.should();
+const chai = require('chai');
+const chathttp = require('chai-http');
+const server = require('../../app');
+const driver = require('../../config/database');
+const body = require('../test_resources/sampleTownCatagory.json');
+
+const should = chai.should();
 chai.use(chathttp);
-let port = 3000;
-let baseUrl = `http://localhost:${port}/`
 
-let resetDB = async function () {
-    let session = driver.session();
+async function resetDB() {
+  const session = driver.session();
 
-    await session.writeTransaction( tx => 
-        tx.run(
-`
-        MATCH (n:LocationCategory)
-        DETACH DELETE n
-`)
-    )
+  await session.writeTransaction((tx) => tx.run(
+    `
+      MATCH (n:LocationCategory)
+      DETACH DELETE n
+    `,
+  ));
 }
 
-describe('/towns/categories',()=>{
-    before((done)=>{
-        console.log("Running test setup")
-        resetDB();
-        done();
-    })
+describe('/towns/categories', () => {
+  before((done) => {
+    console.log('Running test setup');
+    resetDB();
+    done();
+  });
 
-    after((done)=>{
-        console.log("Clearing DB")
-        resetDB();
-        done();
-    })
+  after((done) => {
+    console.log('Clearing DB');
+    resetDB();
+    done();
+  });
 
-    describe('GET', ()=>{
-        it('should return an empty list of catagories', (done)=>{
-            chai.request(server)
-                .get('/towns/categories')
-                .end((err, res)=>{
-                    should.not.exist(err);
-                    should.exist(res);
-                    res.should.have.status(200);
-                    res.body.should.be.an('array')
-                        .and.have.lengthOf(0);
-                    done();
-                })
-        })
-    })
+  describe('GET', () => {
+    it('should return an empty list of catagories', (done) => {
+      chai.request(server)
+        .get('/towns/categories')
+        .end((err, res) => {
+          should.not.exist(err);
+          should.exist(res);
+          res.should.have.status(200);
+          res.body.should.be.an('array')
+            .and.have.lengthOf(0);
+          done();
+        });
+    });
+  });
 
-    describe('POST', ()=>{
-        let body = require('../test_resources/sampleTownCatagory.json');
-        it('should return a list of town ids', (done)=>{
-            chai.request(server)
-                .post('/towns/categories')
-                .set('content-type', 'application/json')
-                .send(body)
-                .end((err, res)=>{
-                    should.not.exist(err);
-                    should.exist(res);
-                    res.should.have.status(201);
-                    res.body.should.be.an('array')
-                        .and.have.lengthOf(body.categories.length);
-                    res.body[0].should.have.deep.property('uuid').and.be.a('string');
-                    done();
-                })
-        })
+  describe('POST', () => {
+    it('should return a list of town ids', (done) => {
+      chai.request(server)
+        .post('/towns/categories')
+        .set('content-type', 'application/json')
+        .send(body)
+        .end((err, res) => {
+          should.not.exist(err);
+          should.exist(res);
+          res.should.have.status(201);
+          res.body.should.be.an('array')
+            .and.have.lengthOf(body.categories.length);
+          res.body[0].should.have.deep.property('uuid').and.be.a('string');
+          done();
+        });
+    });
 
-        it('should return the new catagories in a get request', (done)=>{
-            chai.request(server)
-            .get('/towns/categories')
-            .end((err, res)=>{
-                should.not.exist(err);
-                should.exist(res);
-                res.should.have.status(200);
-                res.body.should.be.an('array')
-                    .and.have.lengthOf(3);
-                done();
-            })
-        })
-    })
-})
+    it('should return the new catagories in a get request', (done) => {
+      chai.request(server)
+        .get('/towns/categories')
+        .end((err, res) => {
+          should.not.exist(err);
+          should.exist(res);
+          res.should.have.status(200);
+          res.body.should.be.an('array')
+            .and.have.lengthOf(3);
+          done();
+        });
+    });
+  });
+});
